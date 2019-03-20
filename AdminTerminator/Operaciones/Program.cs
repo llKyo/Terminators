8507425ed9226e9ae86098421882d0dd63c52f1d
llKyo <k.trivinod@gmail.com>
2019-03-20 18:13:44 -0300

    Término del ingresado del Terminator

--- AdminTerminator/Operaciones/Program.cs
+++ AdminTerminator/Operaciones/Program.cs
@@ -12,9 +12,11 @@ namespace AdminTerminator
     {
         static void MostrarTerminators()
         {
+            //TODO: Mejorar formato de tabla
             List<Terminator> terminators = new TerminatorDAL().ObtenerTerminators();
             terminators.ForEach(t => Console.WriteLine(
                 "Número de Serie:{0} Tipo:{1} Objetivo:{2}", t.NroSerie, t.Tipo, t.Objetivo));
+            Console.ReadKey();
         }
         static void BuscarTerminator()
         {
@@ -23,14 +25,15 @@ namespace AdminTerminator
                 .FiltrarTerminator(Console.ReadLine().Trim())
                 .ForEach(t => Console.WriteLine(
                     "Número de Serie:{0} Tipo:{1} Objetivo:{2}", t.NroSerie, t.Tipo, t.Objetivo));
+            Console.ReadKey();
         }
         static void IngresarTerminator()
         {
             string nroSerie;
-            string tipo;
-            int prioridadBase;
-            string objetivo;
-            Int32 añoDestino;
+            string tipo = "";
+            int prioridadBase = 0;
+            string objetivo = "";
+            Int32 añoDestino = 0;
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.DarkRed;
@@ -83,16 +86,16 @@ namespace AdminTerminator
                 switch (Console.ReadLine().Trim())
                 {
                     case "1":
-                        nroSerie = "T-1";
+                        tipo = "T-1";
                         break;
                     case "2":
-                        nroSerie = "T-800";
+                        tipo = "T-800";
                         break;
                     case "3":
-                        nroSerie = "T-1000";
+                        tipo = "T-1000";
                         break;
                     case "4":
-                        nroSerie = "T-3000";
+                        tipo = "T-3000";
                         break;
                     default:
                         Console.WriteLine("[!] Ingrese una opción válida");
@@ -103,14 +106,35 @@ namespace AdminTerminator
             //FIN TIPO
 
             //PRIORIDADBASE
-
+            do
+            {
+                try
+                {
+                    error = false;
+                    Console.Write("Ingrese Prioridad base: ");
+                    prioridadBase = Int32.Parse(Console.ReadLine().Trim());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("[!] Ingrese un número");
+                    error = true;
+                }
+            } while (error);
             //FIN PRIORIDADBASE
 
             //OBJETIVO
             do
             {
                 error = false;
+                
             } while (error);
+            Console.Write("Ingrese objetivo: ");
+            objetivo = Console.ReadLine().Trim();
+            if (objetivo.ToLower() == "sarah connor")
+            {
+                prioridadBase = 999;
+                Console.WriteLine("[•] La prioridad a cambiado a 999");
+            }
             //FIN OBJETIVO
 
             //AÑO DESTINO
@@ -121,9 +145,10 @@ namespace AdminTerminator
                     error = false;
                     Console.Write("ingrese año de destino: ");
                     añoDestino = Int32.Parse(Console.ReadLine().Trim());
-                    if (1997 < añoDestino || añoDestino < 3000)
+                    if (1997 >= añoDestino || añoDestino >= 3000)
                     {
-                        throw new Exception ();
+                        error = true;
+                        Console.WriteLine("[!] El año de destino debe ser entre 1997 y 3000");
                     }
                 }
                 catch (FormatException ex)
@@ -131,14 +156,16 @@ namespace AdminTerminator
                     error = true;
                     Console.WriteLine("[!] Debe ingresar un número");
                 }
-                catch (Exception ex)
-                {
-                    error = true;
-                    Console.WriteLine("[!] El año de destino debe ser entre 1997 y 3000");
-                }
             } while (error);
             //FIN AÑO DESTINO
 
+            Terminator t = new Terminator() {
+                NroSerie = nroSerie, Tipo = tipo, PrioridadBase = prioridadBase
+                , Objetivo = objetivo, AñoDestino = añoDestino};
+
+            new TerminatorDAL().AgregarTerminator(t);
+            Console.WriteLine("\n[•] Terminator Ingresado.");
+            Console.ReadKey();
             
 
         }
--- AdminTerminator/Program.cs
+++ AdminTerminator/Program.cs
@@ -15,6 +15,7 @@ namespace AdminTerminator
         static bool Menu()
         {
             bool continuar = true;
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("---------- TERMINATORS! ----------");
             Console.WriteLine("1. Ingresar");
@@ -25,21 +26,26 @@ namespace AdminTerminator
             switch (Console.ReadLine().Trim())
             {
                 case "1":
+                    Console.Clear();
                     IngresarTerminator();
                     break;
                 case "2":
+                    Console.Clear();
                     MostrarTerminators();
                     break;
                 case "3":
+                    Console.Clear();
                     BuscarTerminator();
                     break;
                 case "0":
+                    Console.Clear();
                     continuar = false;
                     break;
                 default:
                     Console.WriteLine("Apreta bien ctm");
                     break;
             }
+            
             return continuar;
         }
     }
